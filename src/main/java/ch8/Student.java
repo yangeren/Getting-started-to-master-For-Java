6dce1cb659ba545d12205faca5af82711e6e031f
hanz <xiaoyaoyangeren@163.com>
2017-10-30 23:57:47 +0800

    第8章

---
+++ src/main/java/ch8/Student.java
@@ -0,0 +1,31 @@
+package ch8;
+
+public class Student {
+    private String name;
+    private boolean male;
+    private double account;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isMale() {
+        return male;
+    }
+
+    public void setMale(boolean male) {
+        this.male = male;
+    }
+
+    public double getAccount() {
+        return account;
+    }
+
+    public void setAccount(double account) {
+        this.account = account;
+    }
+}
