e3ffcdafa658b777cb3800e98a648f147ce98ae7
hanz <xiaoyaoyangeren@163.com>
2017-11-02 16:20:09 +0800

    ch14多线程

--- src/main/java/ch15/ThreadTest.java
+++ src/main/java/ch15/ThreadTest.java
@@ -1,8 +1,19 @@
 package ch15;
 
 public class ThreadTest extends Thread{
+    private int count = 100;
+
     @Override
     public void run() {
-        super.run();
+        while (true) {
+            System.out.println(count + "  ");
+            if (--count == 0) {
+                return;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        new ThreadTest().start();
     }
 }
